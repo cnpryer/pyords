f6f7566b8ba253c009894a78a0bcd1a12ba845b1
Chris Pryer <chris.pryer@nfiindustries.com>
2019-05-07 17:32:00 -0400

    setting up dev for routing engine

--- fyords/constraint_solver/routing.py
+++ fyords/constraint_solver/routing.py
@@ -5,16 +5,28 @@ adherence to a plethora of regulations.
 TODO:
     -Eventually improve upon single-depot limitations.
 '''
+import numpy as np
 
 class DedicatedFleet:
     '''
     Purpose:
         DedicatedFleet optimizes operations requiring routes for a
-        dedicated fleet transporting goods.
+        dedicated fleet transporting goods. Dedicated fleets inherently must
+        return to their origins.
     '''
-    def __init__(self):
-        pass
-
+    def __init__(self, distances:np.array, windows:np.array, demands:np.array,
+    vehicles:np.array):
+        '''
+        Args:
+            distances: matrix of all-to-all distances (defines order).
+            times: array of time windows for each location (follows order).
+            demands: array of demands for each location.
+            vehicles: array of capacities representing a vehicle.
+        '''
+        self.distances = distances
+        self.windows = windows
+        self.demands = demands
+        self.vehicles = vehicles
 
 class AmbiguousFleet:
     '''
---
+++ testing/constraint_solver/routing.py
@@ -0,0 +1,54 @@
+from fyords.constraint_solver.routing import DedicatedFleet
+from fyords.preprocess.routing import haversine_distance_matrix
+import pandas as pd
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    # generate testing lat and lon data
+    n = 10
+    data = pd.DataFrame({
+        'origin_lat': np.random.uniform(low=-100.0, high=100.0, size=(n,)),
+        'origin_lon': np.random.uniform(low=-100.0, high=100.0, size=(n,)),
+        'dest_lat': np.random.uniform(low=-100.0, high=100.0, size=(n,)),
+        'dest_lon': np.random.uniform(low=-100.0, high=100.0, size=(n,)),
+        'demand': np.random.randint(low=1, high=10, size=(n,))
+        # TODO: windows and other constraints
+    })
+
+    # build locations list starting with origin (TODO: preprocessing multi-
+    # origin routing models)
+    locations = list([data[['origin_lat', 'origin_lon']].iloc[0].tolist()])
+
+    # aggregate demand
+    data = data.groupby(['origin_lat', 'origin_lon', 'dest_lat', 'dest_lon'])\
+        .sum().reset_index()
+
+    # append destinations with demand to locations
+    locations += list(zip(data.dest_lat.tolist(), data.dest_lon.tolist()))
+    locations = np.array([np.array(loc) for loc in locations]) # convert tuples
+
+    # build demands
+    demands = np.array([0] + data.demand.tolist())
+
+    # build distance matrix
+    lats = np.array([loc[0] for loc in locations])
+    lons = np.array([loc[1] for loc in locations])
+    distances = np.array(haversine_distance_matrix(lats, lons, 'mi'))
+
+    # TODO: build time windows
+    windows = np.array([np.nan for loc in locations])
+
+    # build vehicles
+    vehicles = np.array([45 for i in range(0, 2)])
+
+    routes = DedicatedFleet(
+        distances=distances,
+        windows=windows,
+        demands=demands,
+        vehicles=vehicles
+    )
+
+    print('TESTING')
+    print(', '.join("%s: %s" % item for item in vars(routes).items()))
