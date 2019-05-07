bbcfe3be63ae1bcdb3c142f27150084bd0b24867
Chris Pryer <chris.pryer@nfiindustries.com>
2019-05-07 14:43:14 -0400

    setup opportunity describing helper objects for logistic analysis

---
+++ fyords/describe/analysis.py
@@ -0,0 +1,63 @@
+class BackhaulTransporation:
+    '''
+    Purpose:
+        Identify and describe backhaul transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class ContinuousMovesTransportation:
+    '''
+    Purpose:
+        Identify and describe continuous moves transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class FinalMileTransportation:
+    '''
+    Purpose:
+        Identify and describe final mile transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class DedicatedTransportation:
+    '''
+    Purpose:
+        Identify and describe dedicated transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class IntermodalTransportation:
+    '''
+    Purpose:
+        Identify and describe intermodal transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class MultiStop:
+    '''
+    Purpose:
+        Identify and describe multistop transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class MultiPick:
+    '''
+    Purpose:
+        Identify and describe multipick transportation opportunities.
+    '''
+    def __init__(self):
+        pass
+
+class ActivityLeveling:
+    '''
+    Purpose:
+        Identify and describe activity leveled transportation opportunities.
+    '''
+    def __init__(self):
+        pass
