5fb944298c0433f0727e2558bbc32e663f7b0fac
chris pryer <christophpryer@gmail.com>
2019-06-03 23:10:16 -0400

    ignoring llama dev

--- fyords/helpers/preprocess/__init__.py
+++ fyords/helpers/preprocess/__init__.py
@@ -2,4 +2,3 @@ from .distance import haversine, haversine_vectorized
 from .routing import (
     haversine_distance_matrix,
     encode_random_dedicatedfleet_ga)
-from .scgx import LlamaLoader, LlamaStage
