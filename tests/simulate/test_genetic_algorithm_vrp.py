433318c9e8f90f33b8824df24091e2ba28bb3549
christopherpryer <christophpryer@gmail.com>
2019-09-16 14:15:46 +0100

    corrected genetic algorithm integration for circleci testing

--- tests/simulate/test_genetic_algorithm_vrp.py
+++ tests/simulate/test_genetic_algorithm_vrp.py
@@ -10,16 +10,45 @@ import random
 root_dir = path.dirname(path.abspath(__name__))
 instance_dir = path.join(root_dir, 'instance')
 
-# user-defined parameters
-filepath = path.join(instance_dir, 'demand.csv')
-demand_data = pd.read_csv(filepath) # data to use for solve
-n_generations = 10
+n_generations = 1
 population_size = 10
 
 # each index position of the first individual maps to same position in
 # its environment data (in this case demand_data).
-n = len(demand_data)
+n = 100
 initial_route_ids = np.random.randint(0, n-1, n) # random first individual
+demand_data = pd.DataFrame({
+    'zipcode': np.random.randint(0, n, size=(n,)),
+    'latitude': np.random.uniform(low=40.0, high=41.0, size=(n,)),
+    'longitude': np.random.uniform(low=-78.0, high=-77.0, size=(n,)),
+    'weight': np.random.randint(low=5000, high=45000, size=(n,)),
+    'pallets': np.random.randint(low=1, high=20, size=(n,))})
+
+# set up individuals' environment with dataframe for the solve and
+# {'zip_lookup': df, 'distance_matrix': []} to use within fitness_func.
+# create a lookup for unique zip code positions in distance_matrix
+geo_lookup = demand_data[['zipcode', 'latitude', 'longitude']]\
+    .drop_duplicates()
+geo_lookup['position'] = list(range(len(geo_lookup)))
+
+cols = ['zipcode', 'position']
+environment_dict = {'zip_lookup': geo_lookup.reset_index()[cols]}
+
+# create corresponding distance matrix
+distance_matrix = []
+indicies = geo_lookup.index.tolist()
+for i in indicies:
+    o_lat = geo_lookup.latitude.loc[i]
+    o_lon = geo_lookup.longitude.loc[i]
+    tmp_dist_li = []
+    for j in indicies:
+        d_lat = geo_lookup.latitude.loc[j]
+        d_lon = geo_lookup.longitude.loc[j]
+        dist = haversine((o_lat, o_lon), (d_lat, d_lon), Unit.MILES)
+        tmp_dist_li.append(dist*1.17) # assumed circuity
+    distance_matrix.append(tmp_dist_li)
+environment_dict['distance_matrix'] = distance_matrix
+
 
 def fitness_func(individual, environment):
     """Return a fitness score for an individual. Lower scores rank
@@ -40,28 +69,28 @@ def fitness_func(individual, environment):
 
     # tally penalties (dif from maxing out capacity + minimizing distance)
     weight_penalty = (
-        max_weight - decoded.groupby('chromosomes')['Weight'].sum()
+        max_weight - decoded.groupby('chromosomes')['weight'].sum()
         ).abs().sum()
 
     pallet_penalty = (
-        max_pallets - decoded.groupby('chromosomes')['Plts'].sum()
+        max_pallets - decoded.groupby('chromosomes')['pallets'].sum()
         ).abs().sum()
 
     def get_distance(x):
-        if pd.isnull(x['1St FrZip']) or pd.isnull(x.prev_zip):
+        if pd.isnull(x['zipcode']) or pd.isnull(x.prev_zip):
             return np.nan
         if x.chromosomes != x.prev_chromosomes:
             return np.nan
         lookup = environment._dict['zip_lookup']
-        origin_condition = (lookup['1St FrZip'] == x.prev_zip)
+        origin_condition = (lookup['zipcode'] == x.prev_zip)
         origin_index = lookup.loc[origin_condition, 'position'].values[0]
-        dest_condition = (lookup['1St FrZip'] == x['1St FrZip'])
+        dest_condition = (lookup['zipcode'] == x['zipcode'])
         dest_index = lookup.loc[dest_condition, 'position'].values[0]
         return environment._dict['distance_matrix'][origin_index][dest_index]
 
     def get_distance_penalty():
         decoded.sort_values(by='chromosomes', inplace=True)
-        decoded['prev_zip'] = decoded['1St FrZip'].shift()
+        decoded['prev_zip'] = decoded['zipcode'].shift()
         decoded['prev_chromosomes'] = decoded.chromosomes.shift()
         decoded['distance'] = decoded.apply(get_distance, axis=1)
         return decoded.distance.sum()
@@ -69,41 +98,14 @@ def fitness_func(individual, environment):
     distance_penalty = get_distance_penalty()
     return weight_penalty + pallet_penalty + distance_penalty
 
-# set up individuals' environment with dataframe for the solve and
-# {'zip_lookup': df, 'distance_matrix': []} to use within fitness_func.
-# create a lookup for unique zip code positions in distance_matrix
-geo_lookup = demand_data[['1St FrZip', 'latitude', 'longitude']]\
-    .drop_duplicates()
-geo_lookup['position'] = list(range(len(geo_lookup)))
-
-cols = ['1St FrZip', 'position']
-_dict = {'zip_lookup': geo_lookup.reset_index()[cols]}
-
-# create corresponding distance matrix
-distance_matrix = []
-indicies = geo_lookup.index.tolist()
-for i in indicies:
-    o_lat = geo_lookup.latitude.loc[i]
-    o_lon = geo_lookup.longitude.loc[i]
-    tmp_dist_li = []
-    for j in indicies:
-        d_lat = geo_lookup.latitude.loc[j]
-        d_lon = geo_lookup.longitude.loc[j]
-        dist = haversine((o_lat, o_lon), (d_lat, d_lon), Unit.MILES)
-        tmp_dist_li.append(dist*1.17) # assumed circuity
-    distance_matrix.append(tmp_dist_li)
-_dict['distance_matrix'] = distance_matrix
-
-environment = envs.BasicEnvironment(df=demand_data, _dict=_dict)
-
 # configure algorithm
+environment = envs.BasicEnvironment(df=demand_data, _dict=environment_dict)
 algorithm = algs.BasicGeneticAlgorithm(
     first_individual=initial_route_ids,
     environment=environment,
     fitness_func=fitness_func,
     n_generations=n_generations,
     population_size=population_size)
-
 
 def test_algorithm():
     result = algorithm.run()
