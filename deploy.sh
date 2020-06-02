219e595e7d915c675246fdf2d97a99bca936783f
chris pryer <christophpryer@gmail.com>
2020-06-01 21:37:42 -0400

    ubuntu refactor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -rf build dist *.egg-info
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
+python3 setup.py sdist bdist_wheel
+pip3 install twine
+python3 -m twine upload dist/*
