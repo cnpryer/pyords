93f139d19759da29569f467ffbb18ca845ee7435
chris pryer <christophpryer@gmail.com>
2020-06-07 22:29:48 -0400

    updated for new deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,8 @@
 @echo off
 
+call pip install wheel
 rm -rf build dist *.egg-info
 call python setup.py sdist bdist_wheel
+
+call pip install twine
 call python -m twine upload dist/* --verbose
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
+pip install wheel
 rm -rf build dist *.egg-info
-python3 setup.py sdist bdist_wheel
-pip3 install twine
-python3 -m twine upload dist/*
+python setup.py sdist bdist_wheel
+pip install twine
+python -m twine upload dist/*
