47bc24c4ffdf69f42c9750ff6b80e9cf38be5c6a
chris pryer <christophpryer@gmail.com>
2020-06-11 20:53:55 -0400

    refactored ortools sandbox notebook for potential lib implementation

--- examples/notebooks/py/cp_ortools_sandbox.py
+++ examples/notebooks/py/cp_ortools_sandbox.py
@@ -80,19 +80,18 @@ get_plot(df)
 
 from haversine import haversine, Unit
 
-# select an origin node
-origin_lon, origin_lat = 41.4191, -87.7748
-origins = [(origin_lon, origin_lat)]
+def get_distance_matrix_from_dataframe(origins:list, dataframe:pd.DataFrame):
+    # select an origin node
 
-distances = []
-for coords0 in origins + list(zip(df.latitude, df.longitude)):
-    row = []
-    for coords1 in origins + list(zip(df.latitude, df.longitude)):
-        distance = haversine(coords0, coords1, unit=Unit.MILES)
-        row.append(distance)
-    distances.append(row)
-
-assert len(distances) == len(origins) + len(df)
+    distances = []
+    for coords0 in origins + list(zip(dataframe.latitude, dataframe.longitude)):
+        row = []
+        for coords1 in origins + list(zip(df.latitude, df.longitude)):
+            distance = haversine(coords0, coords1, unit=Unit.MILES)
+            row.append(distance)
+        distances.append(row)
+        
+    return distances
 
 
 # ### Additional Processing
@@ -125,78 +124,121 @@ from ortools.constraint_solver import pywrapcp
 
 # process vehicles input using total destination nodes count
 # i.e. one truck available per stop
-vehicles = [26 for i in range(len(distances[1:]))]
 
-demand = np.insert(df.pallets.values, 0, 0) # using pallets & adding 0 for the depot
-max_solve_seconds = 30
-depot_index = 0
 
 # constructing the model
-manager = pywrapcp.RoutingIndexManager(len(distances), len(vehicles), 0)
+def get_manager(distances:list, vehicles:list, depot_index:int): 
+    return pywrapcp.RoutingIndexManager(len(distances), len(vehicles), 0)
 
-def distance_callback(i:int, j:int):
-    """index of from (i) and to (j)"""
-    node_i = manager.IndexToNode(i)
-    node_j = manager.IndexToNode(j)
-    
-    return distances[node_i][node_j]
-
-model = pywrapcp.RoutingModel(manager)
-
-# add distance constraint
-model.SetArcCostEvaluatorOfAllVehicles(
-    model.RegisterTransitCallback(distance_callback)
-)
-
-def demand_callback(i:int):
-    """capacity constraint"""
-    node = manager.IndexToNode(i)
-    
-    return demand[node]
-
-# add demand constraint
-model.AddDimensionWithVehicleCapacity(
-    # function which return the load at each location (cf. cvrp.py example)
-    model.RegisterUnaryTransitCallback(demand_callback),
-    0, # null capacity slack
-    np.array([cap for cap in vehicles]), # vehicle maximum capacity
-    True, # start cumul to zero
-    'Capacity'
-)
+def get_model(manager):
+    return pywrapcp.RoutingModel(manager)
 
 # config for optimization search
-search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-search_parameters.first_solution_strategy =     routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-search_parameters.time_limit.seconds = max_solve_seconds
+def get_search_params(max_solve_seconds:int=30):
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy =         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    search_parameters.time_limit.seconds = max_solve_seconds
+    
+    return search_parameters
 
-assignment = model.SolveWithParameters(search_parameters)
+class MyPipe:
+    def __init__(self, manager, model):
+        self.manager = manager
+        self.model = model
+        self.model.SetArcCostEvaluatorOfAllVehicles(
+            model.RegisterTransitCallback(self.distance_callback)
+        )
+        self.vehicles = None
+        self.demand = None
+        
+    def distance_callback(self, i:int, j:int):
+        """index of from (i) and to (j)"""
+        node_i = self.manager.IndexToNode(i)
+        node_j = self.manager.IndexToNode(j)
 
-def get_solution():
-    total_distance = 0
-    total_load = 0
-    solution = []
-    for vehicle in range(len(vehicles)):
-        i = model.Start(vehicle)
-        info = {'vehicle': vehicle, 'stops': list(), 'stop_distances': [0],
-                'stop_loads': list()}
+        return distances[node_i][node_j]
+    
+    def demand_callback(self, i:int):
+        """capacity constraint"""
+        node = self.manager.IndexToNode(i)
 
-        while not model.IsEnd(i):
-            node = manager.IndexToNode(i)
-            info['stops'].append(node)
-            info['stop_loads'].append(demand[node])
+        return self.demand[node]
+    
+    def set_demand_callback(self):
+        # add demand constraint using vehicles
+        self.model.AddDimensionWithVehicleCapacity(
+            # function which return the load at each location (cf. cvrp.py example)
+            self.model.RegisterUnaryTransitCallback(self.demand_callback),
+            0, # null capacity slack
+            np.array([cap for cap in self.vehicles]), # vehicle maximum capacity
+            True, # start cumul to zero
+            'Capacity'
+        )
+        
+        return self
+    
+    def add_vehicles(self, vehicles:list):
+        self.vehicles = vehicles
+        if self.demand: 
+            return self.set_demand_callback()
+        
+        return self
+    
+    def add_demand(self, demand:list):
+        self.demand = demand
+        if self.vehicles: 
+            return self.set_demand_callback()
+        
+        return self
+    
+    def get_solution(self, assignment):
+        total_distance = 0
+        total_load = 0
+        solution = []
+        for vehicle in range(len(self.vehicles)):
+            i = self.model.Start(vehicle)
+            info = {'vehicle': vehicle, 'stops': list(), 'stop_distances': [0],
+                    'stop_loads': list()}
+
+            while not self.model.IsEnd(i):
+                node = self.manager.IndexToNode(i)
+                info['stops'].append(node)
+                info['stop_loads'].append(self.demand[node])
+
+                previous_i = int(i)
+                i = assignment.Value(self.model.NextVar(i))
+                info['stop_distances'].append(self.model.GetArcCostForVehicle(previous_i, i, vehicle))
+
+            # add return to depot to align with solution data
+            info['stops'].append(0)
+            info['stop_loads'].append(0)
+            solution.append(info)
+        
+        return solution
             
-            previous_i = int(i)
-            i = assignment.Value(model.NextVar(i))
-            info['stop_distances'].append(model.GetArcCostForVehicle(previous_i, i, vehicle))
+    def run(self, search_params):
+        return self.model.SolveWithParameters(search_params)
+    
+def get_solution_from_dataframe(dataframe:pd.DataFrame):
+    # TODO: abstraction & testing
+    origins = [(41.4191, -87.7748)] # assumed depot location for one-depot solutions
+    distances = get_distance_matrix_from_dataframe(origins, dataframe)
+    assert len(distances) == len(origins) + len(dataframe)
+    
+    vehicles = [26 for i in range(len(distances[1:]))]
+    demand = np.insert(dataframe.pallets.values, 0, 0) # using pallets & adding 0 for the depot
+    max_solve_seconds = 30
+    depot_index = 0
+    
+    manager = get_manager(distances, vehicles, depot_index)
+    model = get_model(manager)
+    pipe = MyPipe(manager, model)
+    search_params = get_search_params()
+    assignment = pipe.add_vehicles(vehicles).add_demand(demand).run(search_params) 
+    
+    return pipe.get_solution(assignment)
         
-        # add return to depot to align with solution data
-        info['stops'].append(0)
-        info['stop_loads'].append(0)
-        solution.append(info)
-        
-    return solution
-
-solution = get_solution()
+solution = get_solution_from_dataframe(df)
 
 assert len(solution) > 0 # TODO: create better solution testing
 
@@ -205,7 +247,7 @@ vehicles_w_loads = [v for v in solution if sum(v['stop_loads']) > 0]
 print('total vehicles: %s' % len(solution))
 print('total vehicles w loads: %s' % len(vehicles_w_loads))
 #print('total load: %s' % solution[-1])
-print('total input load: %s' % demand.sum())
+#print('total input load: %s' % demand.sum())
 print('max stop sequence: %s' % solution[vehicleindex_w_moststops]['stops'])
 
 
@@ -213,18 +255,6 @@ print('max stop sequence: %s' % solution[vehicleindex_w_moststops]['stops'])
 # **Scoring** a solution against a standardized formula/method will allow for more comprehensive testing, debugging, and model tuning. Scoring should be broken down into a **theory score** and a **practical score**. Theory scores will utilize theoretical expectations of vrp solutions (some assumptions about implementations needs to be made). The practical score will measure how implementable a solution is with respect to certain real-world expectations.  
 # 
 # For the sake of simplicity I'll use pandas as my target format and align scoring to get functions that pull from standard solution structs.
-
-# In[ ]:
-
-
-distance_factor
-
-
-# In[ ]:
-
-
-get_load_factor(solution)
-
 
 # In[ ]:
 
@@ -286,26 +316,31 @@ def score_erratic_distance_factor(dataframe:pd.DataFrame):
 def score_state_crossing_factor(dataframe:pd.DataFrame):
     return None
 
+def postprocess_dataframe(dataframe:pd.DataFrame):
+    # TODO: abstraction & testing
+
+    load_factor = score_load_factor(dataframe)
+    assert load_factor == get_load_factor(solution)
+
+    distance_factor = score_distance_factor(dataframe)
+    assert distance_factor == get_distance_factor(solution)
+
+    stop_travel_factor = score_travel_factor(dataframe)
+    multistop_factor = score_multistop_factor(dataframe)
+    satisfaction_factor = score_multistop_factor(dataframe)
+    erratic_distance_factor = score_erratic_distance_factor(dataframe)
+    crossstate_factor = score_state_crossing_factor(dataframe)
+
+    print('load_factor:', load_factor)
+    print('distance_factor:', distance_factor)
+    print('stop_travel_factor:', stop_travel_factor)
+    print('multistop_factor:', multistop_factor)
+    print('satisfaction_factor:', satisfaction_factor)
+    print('erratic_distance_factor:', erratic_distance_factor)
+    print('crossstate_factor:', crossstate_factor)
+    
+    return get_plot(dataframe, 'vehicle')
+
 df = process_solution_to_dataframe(solution, df)
-
-load_factor = score_load_factor(df)
-assert load_factor == get_load_factor(solution)
-
-distance_factor = score_distance_factor(df)
-assert distance_factor == get_distance_factor(solution)
-
-stop_travel_factor = score_travel_factor(df)
-multistop_factor = score_multistop_factor(df)
-satisfaction_factor = score_multistop_factor(df)
-erratic_distance_factor = score_erratic_distance_factor(df)
-crossstate_factor = score_state_crossing_factor(df)
-
-print('load_factor:', load_factor)
-print('distance_factor:', distance_factor)
-print('stop_travel_factor:', stop_travel_factor)
-print('multistop_factor:', multistop_factor)
-print('satisfaction_factor:', satisfaction_factor)
-print('erratic_distance_factor:', erratic_distance_factor)
-print('crossstate_factor:', crossstate_factor)
-get_plot(df, 'vehicle')
+postprocess_dataframe(df)
 
